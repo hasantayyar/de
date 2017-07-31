3762ccf0ad673b7c091e48dc8b4477a5e3424940
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-07-31 09:22:37 +0200

    Nuri abi as a transport

--- data.js
+++ data.js
@@ -27,18 +27,18 @@ var data = [{
 }, {
   "tags": ["tasinma", "ev"],
   "question": "Esya tasiyacagim ama nasil?",
-  "answer": "<ul><li>1. Boris Sar, 0152 340 590 23, facebook.com/boris.sar</li> \
-      <li>2. Girl with a big car - https://www.facebook.com/girlwithabigcar/</li> \
-      <li>3. Van guy Berlin - https://www.facebook.com/vanguyberlin/</li> \
-      <li>4. Mark, 0176 99533563</li> \
-      <li>5. Tal Epstein, 017681226883</li> \
-      <li>6. Piano moving - http://www.pianocenter-berlin.de/</li> \
-      <li>7. Transport Berlin 24h - https://www.facebook.com/transportb...</li> \
-      <li>8. The Van Goes - https://www.facebook.com/thevangoes...</li> \
-      <li>9.  Yousriben Abdelhafidh -  01787904634, https://www.facebook.com/yousriben....</li> \
-      <li>10- Ezequiel Ramone https://www.facebook.com/ezze.ramon...</li> \
-      <li>11. Toni Ford Transit LLLong . 015227543174</li> \
-      <li>12. Mark http://markmanwithavan.tumblr.com/</li><li>13. PONYBOB   https://www.facebook.com/ponybobapp/</li></ul>",
+  "answer": "<ul><li>Nuri Abi Transport +49 163 6674643</li><li>Boris Sar, 0152 340 590 23, facebook.com/boris.sar</li> \
+      <li>Girl with a big car - https://www.facebook.com/girlwithabigcar/</li> \
+      <li>Van guy Berlin - https://www.facebook.com/vanguyberlin/</li> \
+      <li>Mark, 0176 99533563</li> \
+      <li>Tal Epstein, 017681226883</li> \
+      <li>Piano moving - http://www.pianocenter-berlin.de/</li> \
+      <li>Transport Berlin 24h - https://www.facebook.com/transportb...</li> \
+      <li>The Van Goes - https://www.facebook.com/thevangoes...</li> \
+      <li>Yousriben Abdelhafidh -  01787904634, https://www.facebook.com/yousriben....</li> \
+      <li>Ezequiel Ramone https://www.facebook.com/ezze.ramon...</li> \
+      <li>Toni Ford Transit LLLong . 015227543174</li> \
+      <li>Mark http://markmanwithavan.tumblr.com/</li><li>13. PONYBOB   https://www.facebook.com/ponybobapp/</li></ul>",
   "color": "orange"
 }, {
   "tags": ["tasima", "ev"],
